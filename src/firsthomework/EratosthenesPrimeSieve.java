f414cc38ed62e07e08d419f27dc5ce0de434514f
Lukas <lukas.gittmaier@gmail.com>
2020-10-02 15:43:59 +0200

    isPrime

--- src/firsthomework/EratosthenesPrimeSieve.java
+++ src/firsthomework/EratosthenesPrimeSieve.java
@@ -9,11 +9,33 @@ package firsthomework;
  *
  * @author User
  */
-public class EratosthenesPrimeSieve {
+public class EratosthenesPrimeSieve implements PrimeSieve{
     private int o;
 
     public EratosthenesPrimeSieve(int o) {
         this.o = o;
+    }
+
+    @Override
+    public boolean isPrime(int p) {
+        boolean prime = true;
+        
+        if(p <= 1) {
+            prime = false;
+        }else{
+            for(int i = 2; i <= p /2; i++){
+                if((p%i) == 0){
+                    prime = false;
+                    break;
+                }
+            }
+        }
+        return prime;
+    }
+
+    @Override
+    public void printPrime() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
     
