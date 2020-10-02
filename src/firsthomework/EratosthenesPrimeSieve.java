ced026d8673bb4de7561485ac5e9f00a7dd7ad30
Lukas <lukas.gittmaier@gmail.com>
2020-10-02 14:37:56 +0200

    classes

---
+++ src/firsthomework/EratosthenesPrimeSieve.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firsthomework;
+
+/**
+ *
+ * @author User
+ */
+public class EratosthenesPrimeSieve {
+    private int o;
+
+    public EratosthenesPrimeSieve(int o) {
+        this.o = o;
+    }
+    
+    
+}
